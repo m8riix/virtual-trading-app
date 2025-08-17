0d02dc0ac38c4feee040d7146845ca50ca496f6a
m8riix <52785020+m8riix@users.noreply.github.com>
2025-08-17 14:16:50 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://virtual-trading-app-six.vercel.app'],
+  origin: ['http://localhost:3000', 'https://virtual-trading-app-six.vercel.app','https://virtual-trading-mxwfbqv31-m8riixs-projects.vercel.app',/https:\/\/virtual-trading-.*\.vercel\.app$/],
   credentials: true
 }));
 app.use(express.json());
@@ -49,3 +49,4 @@ app.use('/api/watchlist', require('./routes/watchlist'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
