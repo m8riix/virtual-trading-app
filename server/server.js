827950d4c369d1a2aefae723644e18e31fea8f9c
m8riix <52785020+m8riix@users.noreply.github.com>
2025-08-17 10:37:02 +0530

    Update server.js
    Update Backend CORS Configuration

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://virtual-trading-app-six.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
@@ -40,3 +40,4 @@ app.use('/api/watchlist', require('./routes/watchlist'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
