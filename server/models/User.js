872680350ed3f25ee0f19a232581d0f232b30a1c
m8riix <52785020+m8riix@users.noreply.github.com>
2025-08-17 13:47:12 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,42 +1,12 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-// Clear any cached model
-if (mongoose.models.User) {
-  delete mongoose.models.User;
-}
-
-const portfolioSchema = new mongoose.Schema({
-  symbol: { 
-    type: String, 
-    required: [true, 'Symbol is required']
-  },
-  quantity: { 
-    type: Number, 
-    required: [true, 'Quantity is required'],
-    min: [0, 'Quantity must be positive']
-  },
-  buyPrice: { 
-    type: Number, 
-    required: [true, 'Buy price is required'],
-    min: [0, 'Buy price must be positive']
-  },
-  currentPrice: { 
-    type: Number, 
-    default: 0,
-    min: [0, 'Current price must be positive']
-  }
-}, { _id: false });
-
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   balance: { type: Number, default: 1000000 },
-  portfolio: {
-    type: [portfolioSchema],
-    default: []
-  }
+  portfolio: { type: Array, default: [] }
 }, {
   timestamps: true
 });
